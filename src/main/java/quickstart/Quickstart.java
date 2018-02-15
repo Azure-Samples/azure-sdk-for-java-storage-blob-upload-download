cff19e6037020cc5b5ce0127e73a4b823e123ca9
Rikki Gibson <rigibson@microsoft.com>
2018-02-14 19:12:28 -0800

    Cleanup

--- src/main/java/quickstart/Quickstart.java
+++ src/main/java/quickstart/Quickstart.java
@@ -1,10 +1,11 @@
+package quickstart;
+
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.UnsupportedEncodingException;
 import java.io.Writer;
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -12,7 +13,6 @@ import java.nio.ByteBuffer;
 import java.nio.channels.AsynchronousFileChannel;
 import java.nio.charset.StandardCharsets;
 import java.security.InvalidKeyException;
-import java.util.Random;
 
 import com.microsoft.azure.storage.blob.BlobRange;
 import com.microsoft.azure.storage.blob.BlockBlobURL;
@@ -29,11 +29,8 @@ import com.microsoft.rest.v2.util.FlowableUtil;
 
 import io.reactivex.Flowable;
 
-
-public class quickstart {
-    
-    static File createTempFile(){
-        
+public class Quickstart {
+    static File createTempFile() {
         // Here we are creating a temporary file to use for download and upload to Blob storage
         File sampleFile = null;
         try {
@@ -41,42 +38,36 @@ public class quickstart {
             System.out.println(">> Creating a sample file at: " + sampleFile.toString());
             Writer output = new BufferedWriter(new FileWriter(sampleFile));
             output.write("Hello Azure!");
-            output.close();    
+            output.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
-        
+
         return sampleFile;
     }
-    
-    static void uploadFile(BlockBlobURL blob, File sourceFile){
-        
+
+    static void uploadFile(BlockBlobURL blob, File sourceFile) {
         // All APIs accept asynchronous ByteBuffers stored in a Flowable. 
         // Let's use PutBlob to upload the sample data
         try {
             // Read the file asynchronously into a Flowable
             AsynchronousFileChannel fileChannel = AsynchronousFileChannel.open(sourceFile.toPath());
             Flowable<ByteBuffer> stream = FlowableUtil.readFile(fileChannel);
-        
+
             blob.putBlob(stream, sourceFile.length(), null, null, null)
-            .doFinally(() -> {
-                fileChannel.close();
-            }).subscribe(success -> {
-                System.out.println(">> Success : " + success.statusCode() + ", Etag: " + success.headers().eTag());
-            }, error -> {
-                System.out.println(">> Failed to upload the file");
-            });
-            
+            .doFinally(fileChannel::close)
+            .subscribe(
+                success -> System.out.println(">> Success : " + success.statusCode() + ", Etag: " + success.headers().eTag()),
+                error -> System.out.println(">> Failed to upload the file"));
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
-  
-    static void listBlobs(ContainerURL containerURL){
-        
-        // Each ContainerURL.listBlobs call return up to maxResults (maxResults=100 passed into ListBlobOptions below). 
+
+    static void listBlobs(ContainerURL containerURL) {
+        // Each ContainerURL.listBlobs call return up to maxResults (maxResults=10 passed into ListBlobOptions below).
         // To list all Blobs, we are creating a helper static method called listAllBlobs
-        // This method keeps track of nextMarker, and repeat listBlobs call until there is no more nextMarker
+        // This method keeps track of nextMarker, and repeats the listBlobs call until there is no more nextMarker
         // The result of listBlobs is appended into a Flowable
         ListBlobsOptions options = new ListBlobsOptions(null, null, null, 10);
         listAllBlobs(containerURL, null, options)
@@ -93,61 +84,64 @@ public class quickstart {
 
     private static Flowable<ListBlobsResponse> listAllBlobs(ContainerURL url, String marker, ListBlobsOptions options) {
         return url.listBlobs(marker, options) // calling ContainerURL.listBlobs to retrieve the next 100 blobs with the nextMarker
-            .flatMapPublisher(res -> Flowable.just(res.body()).concatWith( // this will repeat until nextMarker is null
-                res.body().nextMarker() != null ? listAllBlobs(url, res.body().nextMarker(), options) : Flowable.empty())); 
+        .flatMapPublisher(res -> Flowable.just(res.body()).concatWith( // this will repeat until nextMarker is null
+                res.body().nextMarker() != null ? listAllBlobs(url, res.body().nextMarker(), options) : Flowable.empty()));
     }
-    
-   static void deleteBlob(BlockBlobURL blobURL){
-        
+
+    static void deleteBlob(BlockBlobURL blobURL) {
         // Delete the blob
         blobURL.delete(null, null)
-        .subscribe(response -> {
-            System.out.println(">> Blob deleted: " + blobURL);
-        }, error -> {
-            System.out.println(">> An error encountered during deleteBlob: " + error.getMessage());
-        }); 
+        .subscribe(
+            response -> System.out.println(">> Blob deleted: " + blobURL),
+            error -> System.out.println(">> An error encountered during deleteBlob: " + error.getMessage()));
 
     }
-   
-   static void getBlob(BlockBlobURL blobURL){
-       
-       // Get the blob
-       // Since the blob is small, we'll read into memory asynchronously
-       // com.microsoft.rest.v2.util.FlowableUtil is a static class that contains helpers to work with Flowable
-       blobURL.getBlob(new BlobRange(0, 1000000000), null, false)
-       .flatMap(res -> FlowableUtil.collectBytesInArray(res.body()))
-       .subscribe(body -> {
-            System.out.println(">> Blob contents: " + new String(body, StandardCharsets.UTF_8));;
-       }, error -> {
-            System.out.println(">> An error encountered during getBlob: " + error.getMessage());
-       }); // getBlob calls the service only when you subscribe to it
 
-   }
-    
+    static void getBlob(BlockBlobURL blobURL) {
+        // Get the blob
+        // Since the blob is small, we'll read the entire blob into memory asynchronously
+        // com.microsoft.rest.v2.util.FlowableUtil is a static class that contains helpers to work with Flowable
+        blobURL.getBlob(new BlobRange(0, 1000000000), null, false)
+        .flatMap(res -> FlowableUtil.collectBytesInArray(res.body()))
+        .subscribe(body -> {
+            System.out.println(">> Blob contents: " + new String(body, StandardCharsets.UTF_8));;
+        }, error -> {
+            System.out.println(">> An error encountered during getBlob: " + error.getMessage());
+        }); // getBlob calls the service only when you subscribe to it
+
+    }
+
     public static void main(String[] args){
-        ContainerURL containerURL = null;
-        
-        //Creating a sample file to use in the sample
+        ContainerURL containerURL;
+
+        // Creating a sample file to use in the sample
         File sampleFile = createTempFile();
 
         try {
             // Retrieve the credentials and initialize SharedKeyCredentials    
             String accountName = System.getenv("AZURE_STORAGE_ACCOUNT");
             String accountKey = System.getenv("AZURE_STORAGE_ACCESS_KEY");
-            
+
             // Create a ServiceURL to call the Blob service. We will also use this to construct the ContainerURL
             SharedKeyCredentials creds = new SharedKeyCredentials(accountName, accountKey);
             final ServiceURL serviceURL = new ServiceURL(new URL("https://" + accountName + ".blob.core.windows.net"), StorageURL.createPipeline(creds, new PipelineOptions()));
-            
+
             // Let's create a container using a blocking call to Azure Storage
-            Random rand = new Random();
             containerURL = serviceURL.createContainerURL("bigdata");
-            //int successCode = containerURL.create(null, null).blockingGet().statusCode();
-            //System.out.println("Container creation succeeded with HTTP Status code: " + successCode );
-            
+            try {
+                containerURL.create(null, null).blockingGet();
+                System.out.println("Created bigdata container");
+            } catch (RestException e) {
+                if (e.response().statusCode() != 409) {
+                    throw e;
+                } else {
+                    System.out.println("bigdata container already exists");
+                }
+            }
+
             // Create a BlockBlobURL to run operations on Blobs
             final BlockBlobURL blobURL = containerURL.createBlockBlobURL("SampleBlob.txt");
-            
+
             // Listening for commands from the console
             System.out.println("Enter a command");
             System.out.println("(P)utBlob | (L)istBlobs | (G)etBlob | (D)eleteBlobs | (E)xitSample");
@@ -170,22 +164,21 @@ public class quickstart {
                     case "G":
                         System.out.println("Get the blob: " + blobURL.toString() );
                         getBlob(blobURL);
-                        break;                 
+                        break;
                     case "D":
                         System.out.println("Delete the blob: " + blobURL.toString() );
                         deleteBlob(blobURL);
                         break;
-                    case "E": 
+                    case "E":
                         System.out.println("Cleaning up the sample and exiting!");
                         containerURL.delete(null).blockingGet();
                         System.exit(0);
                         break;
                     default:
                         break;
-                } 
-             
+                }
             }
-            
+
         } catch (InvalidKeyException e) {
             System.out.println("Invalid Storage account name/key provided");
         } catch (MalformedURLException e) {
@@ -194,7 +187,7 @@ public class quickstart {
             System.out.println("Service error returned: " + e.response().statusCode() );
         } catch (IOException e) {
             e.printStackTrace();
-        } 
+        }
     }
 
 }
